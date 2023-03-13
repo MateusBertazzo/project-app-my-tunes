6391ed72b2c8cb482b90f7020d0b4f05c71276d2
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-03-13 18:03:48 -0300

    resolve req 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
 import { createUser } from '../services/userAPI';
 
 const MIN_NUMBER = 3;
@@ -6,37 +8,49 @@ const MIN_NUMBER = 3;
 class Login extends Component {
   state = {
     name: '',
+    loading: false,
+    logado: false,
   };
 
-  saveInLocalStorage = () => {
-    const { history } = this.props;
+  handleSubmit = async () => {
     const { name } = this.state;
-    createUser({ name });
+    this.setState({
+      loading: true,
+    });
+    await createUser({ name });
+    this.setState({
+      // loading: false,
+      logado: true,
+    });
   };
 
   render() {
-    const { name } = this.state;
+    const { name, loading, logado } = this.state;
+    const form = (
+      <form>
+        <input
+          onChange={ ({ target }) => this.setState({ name: target.value }) }
+          type="text"
+          data-testid="login-name-input"
+          placeholder="Login"
+          minLength="3"
+        />
+
+        <button
+          onClick={ this.handleSubmit }
+          disabled={ name.length < MIN_NUMBER }
+          type="button"
+          data-testid="login-submit-button"
+        >
+          Entrar
+
+        </button>
+      </form>);
+
     return (
       <div data-testid="page-login">
-        <form>
-          <input
-            onChange={ ({ target }) => this.setState({ name: target.value }) }
-            type="text"
-            data-testid="login-name-input"
-            placeholder="Login"
-            minLength="3"
-          />
-
-          <button
-            onClick={ this.saveInLocalStorage }
-            disabled={ name.length < MIN_NUMBER }
-            type="button"
-            data-testid="login-submit-button"
-          >
-            Entrar
-
-          </button>
-        </form>
+        {!loading ? form : <Loading />}
+        {logado && <Redirect to="/search" />}
       </div>
     );
   }
